6327f333020d1a61f29b6df171b1006f67ead274
Gautam Sharma <sharmagautam2811@gmail.com>
2024-01-21 22:53:53 +0530

    React Gym App

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import Logo from '../assets/images/Logo-1.png';
+
+const Footer = () => {
+  
+  return (
+    <Box mt="80px" bgcolor="#FFF3F4">
+      <Stack gap="40px" alignItems='center' px="40px" pt="24px">
+        <img src={Logo} alt="logo" style={{ width: '200px', height: '40px' }} />
+      </Stack>
+      <Typography 
+        variant="h5" 
+        pb='40px'
+        mt="20px" 
+        textAlign="center"
+      >
+        Made by <br/> 
+        Gautam Sharma (PRN : 2130331245057) <br/> 
+        Rushabh Aradwad (PRN : 2130331245056) <br/> 
+        Himanshu Singh (PRN : 2130331245058) 
+      </Typography>
+    </Box>
+  );
+};
+
+export default Footer;
